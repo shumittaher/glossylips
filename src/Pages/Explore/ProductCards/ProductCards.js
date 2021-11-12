423fa395c13ecf4fb678f1926eb1791e9216ff80
shumit <shumitt@gmail.com>
2021-11-13 00:19:35 +0600

    ok

--- src/Pages/Explore/ProductCards/ProductCards.js
+++ src/Pages/Explore/ProductCards/ProductCards.js
@@ -11,25 +11,31 @@ import { Link } from 'react-router-dom';
 
 const ProductCards = ({ product }) => {
 
-    const { productTitle, brand, type, price,  imageLink , _id} = product
+    const { productTitle, brand, type, price, color, imageLink, _id } = product
 
 
     return (
 
-      
-            <Card sx={{
-                maxWidth: 400, mx: 'auto', height: 500, my: 2,
-            }} elevation={3} >
 
-                <CardMedia
-                    component="img"
-                    height="280"
-                    image={imageLink}
-                    alt="green iguana"
-                />
+        <Card sx={{
+            mx: 'auto', height: 500,
+        }} elevation={24} >
+
+            <CardMedia
+                component="img"
+                height="280"
+                image={imageLink}
+                alt="green iguana"
+            />
+
+
+            <Box sx={{
+                display: 'flex',
+                height: 170
+            }}>
 
                 <CardContent sx={{
-                    height: 130, p: 2
+                    width: '70%',
                 }}>
 
                     <Typography gutterBottom variant="h5" component="div">
@@ -76,7 +82,6 @@ const ProductCards = ({ product }) => {
                     </Box>
                     <Box sx={{
                         display: 'flex',
-
                     }}>
                         <Typography
                             sx={{
@@ -92,23 +97,45 @@ const ProductCards = ({ product }) => {
                             align='left' variant="button" display="inline" gutterBottom>
                             BDT   {price}
                         </Typography>
+
+
                     </Box>
 
 
+
                 </CardContent>
-
-                <CardActions
+                <Box
                     sx={{
-                        p: 2
+                        width: '30%',
                     }}>
-                    <Link style={{
-                        textDecoration: 'none',
-                    }} to={`/ordersconfirmation/${_id}`}>
-                        <Button size="small">Order Now</Button>
-                    </Link>
-                </CardActions>
+                    <svg
+                        style={{
 
-            </Card>
+                        }}
+                        viewBox="0 0 10 18" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="85%" cy="55%" r="8" fill={color} />
+                    </svg>
+                </Box>
+            </Box>
+            <CardActions
+                sx={{
+                    height: 35,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundImage: 'linear-gradient(to top right, #e3b3e8, #e8b3c4, #d5b3e8)'
+                }}
+            >
+                <Link
+                    style={{
+                        textDecoration: 'none',
+                    }}
+                    to={`/ordersconfirmation/${_id}`}>
+                    <Button variant="contained"  size="large">Order Now</Button>
+                </Link>
+            </CardActions>
+
+        </Card>
     )
 }
 
